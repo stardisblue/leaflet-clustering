80079f7d2da62f07c03c6d0c51b36b79bc718425
stardisblue <stardisblue@gmail.com>
2024-09-20 22:56:42 +0200

    chore: add padding for demo

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import 'leaflet/dist/leaflet.css';
 import { ClusterFeatureGroup } from '../lib/ClusterFeatureGroup';
 import glottolog from './dataset/glottolog.json';
 
-const map = L.map('map').setView([10.9, 2.3], 6);
+const map = L.map('map').setView([48.9, 2.3], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-new ClusterFeatureGroup(glottolog.map((d) => L.circleMarker(d, { radius: 1 })))
+new ClusterFeatureGroup(
+  glottolog.map((d) => L.circleMarker(d, { radius: 1 })),
+  { padding: 20 }
+)
   .bindPopup((d: any) => d.getRadius() + ' ' + d.getLayers().length)
   .addTo(map);
