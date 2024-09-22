6f4879f1184dc0f57db46edb6a683b84b3bc8f83
stardisblue <stardisblue@gmail.com>
2024-09-22 18:47:29 +0200

    tests (ClusterFeatureGroup): clusters on zoom and depends on radius

--- spec/ClusterFeatureGroup.spec.ts
+++ spec/ClusterFeatureGroup.spec.ts
@@ -1,9 +1,10 @@
-import { circleMarker, CircleMarker, map } from 'leaflet';
+import { circleMarker, CircleMarker, Map, map } from 'leaflet';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ClusterFeatureGroup } from '../lib/ClusterFeatureGroup';
 
 interface Context {
   markers: CircleMarker[];
+  map: Map;
 }
 
 describe('ClusterFeatureGroup', () => {
@@ -15,20 +16,49 @@ describe('ClusterFeatureGroup', () => {
       { id: 'frp', lat: 45, lng: 6 },
       { id: 'frt', lat: -15, lng: 176 },
       { id: 'fry', lat: 55, lng: 5 },
-    ].map((m) => circleMarker(m, { id: m.id } as any));
+    ].map((m) => circleMarker(m));
 
     document.body.innerHTML = '<div id="map"></div>';
+    context.map = map(document.getElementById('map') as any as HTMLElement);
   });
 
-  it<Context>('should use FSAC for creating clusters', ({ markers }) => {
-    const leafletMap = map(
-      document.getElementById('map') as any as HTMLElement
-    ).setView([48.9, 2.3], 3);
+  it<Context>('should use FSAC for creating clusters', ({ markers, map }) => {
+    map.setView([48.9, 2.3], 3);
     const cluster = new ClusterFeatureGroup(markers);
-    cluster.addTo(leafletMap);
+    cluster.addTo(map);
 
     setTimeout(() => {
       expect(cluster.getLayers().length).toEqual(4);
     }, 0);
   });
+
+  it<Context>('should update clustering depending on zoomlevel', ({
+    markers,
+    map,
+  }) => {
+    map.setView([48.9, 2.3], 3);
+    const cluster = new ClusterFeatureGroup(markers);
+    cluster.addTo(map);
+
+    setTimeout(() => {
+      expect(cluster.getLayers().length).toEqual(4);
+      map.setZoom(1);
+      expect(cluster.getLayers().length).toEqual(3);
+    }, 0);
+  });
+
+  it<Context>('should update clustering depending on radius', ({
+    markers,
+    map,
+  }) => {
+    map.setView([48.9, 2.3], 3);
+    markers.forEach((m) => m.setRadius(30));
+    const cluster = new ClusterFeatureGroup(markers);
+    cluster.addTo(map);
+
+    setTimeout(() => {
+      expect(cluster.getLayers().length).toEqual(3);
+    }, 0);
+  });
+
 });
