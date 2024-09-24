e27cb8636d753345254ca3a4b1a9a557631e74f4
stardisblue <stardisblue@gmail.com>
2024-09-24 20:05:28 +0200

    chore (demo): update demo to use RoundDivMarkers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import '../lib/style.css';
+
 import { circleMarker, map, marker, tileLayer } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { ClusterFeatureGroup } from '../lib/ClusterFeatureGroup';
 import glottolog from './dataset/glottolog.json';
+import { RoundDivClusterMarker } from '../lib/RoundDivClusterMarker';
 
 const leafletMap = map('map').setView([48.9, 2.3], 6);
 
@@ -10,9 +13,12 @@ tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(leafletMap);
 
-new ClusterFeatureGroup(
-  glottolog.map((d) => (Math.random() > 0.5 ? circleMarker(d) : marker(d)))
-)
+const clusters = new ClusterFeatureGroup(
+  glottolog.map((d) => (Math.random() > 0.5 ? circleMarker(d) : marker(d))),
+  { ClusterMarker: RoundDivClusterMarker }
+);
+
+clusters
   .bindPopup(
     (d: any) =>
       d.getLatLng() +
