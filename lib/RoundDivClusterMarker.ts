2a1f80bd8e0a8e62814b9c5e4bb1ffc98ac40c41
stardisblue <stardisblue@gmail.com>
2024-10-06 17:22:54 +0200

    refactor (RoundDivClusterMarker): more permissive cluster type

--- lib/RoundDivClusterMarker.ts
+++ lib/RoundDivClusterMarker.ts
@@ -10,57 +10,63 @@ import {
 import { SupportedMarker } from './CircleClusterMarker';
 import { CircleCluster } from './clustering/Circle';
 
-export type RoundDivClusterMarkerOptions = Omit<MarkerOptions, 'icon'> & {
+export type RoundDivClusterMarkerOptions<
+  T extends Pick<CircleCluster, 'radius'> = CircleCluster,
+> = Omit<MarkerOptions, 'icon'> & {
   icon?: (
     layers: SupportedMarker[],
-    cluster: CircleCluster
+    cluster: T
   ) => Omit<DivIconOptions, 'icon' | 'iconSize' | 'iconAnchor'>;
 };
 
-export interface RoundDivClusterMarker extends Marker {
-  _layers: Record<number, SupportedMarker>;
-  new (
+export interface RoundDivClusterMarkerConstructor {
+  new <T extends Pick<CircleCluster, 'radius'> = CircleCluster>(
     latLng: LatLngExpression,
     layers: SupportedMarker[],
-    cluster: CircleCluster,
-    options?: RoundDivClusterMarkerOptions
+    cluster: T,
+    options?: RoundDivClusterMarkerOptions<T>
   ): RoundDivClusterMarker;
+}
+
+export interface RoundDivClusterMarker extends Marker {
+  _layers: Record<number, SupportedMarker>;
   getLayers(): SupportedMarker[];
   getLayerId(layer: SupportedMarker): number;
 }
 
-export const RoundDivClusterMarker: RoundDivClusterMarker = Marker.extend({
-  initialize(
-    this: RoundDivClusterMarker,
-    latLng: LatLngExpression,
-    layers: SupportedMarker[],
-    cluster: CircleCluster,
-    { icon, ...options }: RoundDivClusterMarkerOptions = {}
-  ) {
-    this._layers = Object.fromEntries(
-      Array.from(layers, (l) => [this.getLayerId(l), l])
-    );
-    const iconOptions = icon
-      ? icon(layers, cluster)
-      : {
-          html: '' + layers.length,
-          className: 'leaflet-round-clustering-marker',
-        };
+export const RoundDivClusterMarker: RoundDivClusterMarkerConstructor =
+  Marker.extend({
+    initialize(
+      this: RoundDivClusterMarker,
+      latLng: LatLngExpression,
+      layers: SupportedMarker[],
+      cluster: CircleCluster,
+      { icon, ...options }: RoundDivClusterMarkerOptions = {}
+    ) {
+      this._layers = Object.fromEntries(
+        Array.from(layers, (l) => [this.getLayerId(l), l])
+      );
+      const iconOptions = icon
+        ? icon(layers, cluster)
+        : {
+            html: '' + layers.length,
+            className: 'leaflet-round-clustering-marker',
+          };
 
-    (Marker.prototype as any).initialize.call(this, latLng, {
-      icon: divIcon({
-        ...iconOptions,
-        iconSize: [cluster.radius * 2, cluster.radius * 2],
-      }),
-      ...options,
-    });
-  },
+      (Marker.prototype as any).initialize.call(this, latLng, {
+        icon: divIcon({
+          ...iconOptions,
+          iconSize: [cluster.radius * 2, cluster.radius * 2],
+        }),
+        ...options,
+      });
+    },
 
-  getLayers(this: RoundDivClusterMarker) {
-    return Object.values(this._layers);
-  },
+    getLayers(this: RoundDivClusterMarker) {
+      return Object.values(this._layers);
+    },
 
-  getLayerId(layer: SupportedMarker) {
-    return Util.stamp(layer);
-  },
-}) as any;
+    getLayerId(layer: SupportedMarker) {
+      return Util.stamp(layer);
+    },
+  }) as any;
