049deea5e6a2de2381687be96fb66ff6efa16817
stardisblue <stardisblue@gmail.com>
2024-10-06 16:58:14 +0200

    refactor: move RectangleDivClusterMarker to it's own folder

--- spec/RectangleDivClusterMarker/BehavesLikeAMarker.spec.ts
+++ spec/RectangleDivClusterMarker/BehavesLikeAMarker.spec.ts
@@ -7,14 +7,14 @@ import { describe, expect, it } from 'vitest';
 const cluster = { minX: 10, minY: 10, maxX: 20, maxY: 20 };
 
 describe('Is used the same way as Marker', () => {
-  it('is possible to use map.addLayer(#)', () => {
+  it('can be added with map.addLayer(#)', () => {
     const marker = new RectangleDivClusterMarker([0, 0], [], cluster);
     const leafletMap = map(document.createElement('div')).addLayer(marker);
 
     expect(leafletMap.hasLayer(marker)).toBe(true);
   });
 
-  it('is possible to use #.addTo(map)', () => {
+  it('can be added with #.addTo(map)', () => {
     const leafletMap = map(document.createElement('div'));
     const marker = new RectangleDivClusterMarker([0, 0], [], cluster).addTo(
       leafletMap
@@ -40,13 +40,13 @@ describe('Is used the same way as Marker', () => {
 });
 
 describe('Accepts various LatLng descriptions', () => {
-  it('should accept [lat, lng] format', () => {
+  it('accepts [lat, lng] format', () => {
     const marker = new RectangleDivClusterMarker([5, 5], [], cluster);
 
     expect(marker.getLatLng()).toEqual(latLng(5, 5));
   });
 
-  it('should accept L.latLng(lat, lng) format', () => {
+  it('accepts L.latLng(lat, lng) format', () => {
     const marker = new RectangleDivClusterMarker(latLng(5, 5), [], cluster);
 
     expect(marker.getLatLng()).toEqual(latLng(5, 5));
