2332a4596b79cba3ed8d2e659b667cd2addc0487
stardisblue <stardisblue@gmail.com>
2024-10-06 17:21:15 +0200

    test (RoundDivClusterMarker): should behave like a marker

---
+++ spec/RoundDivClusterMarker/BehavesLikeAMarker.spec.ts
@@ -0,0 +1,67 @@
+// @vitest-environment happy-dom
+
+import { RoundDivClusterMarker } from '@/RoundDivClusterMarker';
+import { latLng, map } from 'leaflet';
+import { describe, expect, it } from 'vitest';
+
+const cluster = { radius: 42 };
+
+describe('Is used the same way as Marker', () => {
+  it('can be added with map.addLayer(#)', () => {
+    const marker = new RoundDivClusterMarker([0, 0], [], cluster);
+    const leafletMap = map(document.createElement('div')).addLayer(marker);
+
+    expect(leafletMap.hasLayer(marker)).toBe(true);
+  });
+
+  it('can be added with #.addTo(map)', () => {
+    const leafletMap = map(document.createElement('div'));
+    const marker = new RoundDivClusterMarker([0, 0], [], cluster).addTo(
+      leafletMap
+    );
+
+    expect(leafletMap.hasLayer(marker)).toBe(true);
+  });
+
+  it('takes Marker properties in account', () => {
+    const optionsSubset = {
+      opacity: 0.5,
+      attribution: 'hello world',
+    };
+    const marker = new RoundDivClusterMarker(
+      [0, 0],
+      [],
+      cluster,
+      optionsSubset
+    );
+
+    expect(marker.options).toMatchObject(optionsSubset);
+  });
+});
+
+describe('Accepts various LatLng descriptions', () => {
+  it('accepts [lat, lng] format', () => {
+    const marker = new RoundDivClusterMarker([5, 5], [], cluster);
+
+    expect(marker.getLatLng()).toEqual(latLng(5, 5));
+  });
+
+  it('accepts L.latLng(lat, lng) format', () => {
+    const marker = new RoundDivClusterMarker(latLng(5, 5), [], cluster);
+
+    expect(marker.getLatLng()).toEqual(latLng(5, 5));
+  });
+});
+
+it('accepts Icon options', () => {
+  const marker = new RoundDivClusterMarker(latLng(5, 5), [], cluster, {
+    icon() {
+      return { className: 'custom-classname', attribution: 'hello world' };
+    },
+  });
+
+  expect(marker.getIcon().options).toMatchObject({
+    className: 'custom-classname',
+    attribution: 'hello world',
+  });
+});
